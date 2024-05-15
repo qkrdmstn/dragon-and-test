c7924510e5487e7798fadcfb34c8108a84d8978a
Sieun Choi <tldmschl81@soongsil.ac.kr>
2024-05-15 17:44:40 +0900

    - 튜토리얼에서 던지는 족보 프리팹 - 튜토리얼 대화 개발중 	-  빠진 재장전 대화 추가 	- 플레이어 상호작용 enum type에 튜토리얼 추가 - 족보 설명 UI 개발중

--- Assets/Scripts/System/UI/UIGroup/TutorialUIGroup.cs
+++ Assets/Scripts/System/UI/UIGroup/TutorialUIGroup.cs
@@ -1,69 +1,63 @@
+
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
+using TMPro;
 using UnityEngine.UI;
 
 public class TutorialUIGroup : UIGroup
 {
     public Vector3 padding;
-    Animator anim;
+    public Transform scarecrowPos;
+    public GameObject jokbo, jokboInstantiate;
+    //Animator anim;
 
-    public bool isWASD, isAttack, isDash, isSkill, isReload, isInteraction; // UI가 뜨고 플레이어가 해당 UI에 대한 key를 누르면 활성화
+    public bool isWASD, isAttack, isDash, isReload, isInteraction;  // variable of SetTutoTxt_1
+    public bool isSkill, isOpenJokbo, isGetJokbo;   // variable of SetTutoTxt_3
+    public bool isScarecrow, isJokbo, isCloseJokbo;
 
     #region DialogueInfo
     public int curIdx;
+    public float waitTime = 0f;
+
     DialogueDB dialogues;
     List<DialogData> dialogDatas = new List<DialogData>(); // 현재 eventName과 동일한 대화DB를 저장할 구조체
-    TextMeshProUGUI npcName;
     TextMeshProUGUI dialogueTxt;
-    bool isDone, isFirst;
+    bool isDone;
+    bool isFirst;
     #endregion
-
-    private void Awake()
-    {
-        padding = new Vector3(0, 175f, 0);
-        anim = GetComponent<Animator>();
-    }
-
-    private void OnEnable()
-    {
-        if (anim == null) return;
-
-        anim.enabled = true;
-    }
 
     private void Update()
     {
-        transform.position = Camera.main.WorldToScreenPoint(GameManager.instance.player.transform.position) + padding;
+        transform.position = Camera.main.WorldToScreenPoint(scarecrowPos.position) + padding;
     }
 
     private void OnDisable()
     {
         isWASD = isAttack = isDash = isSkill = isReload = isInteraction = false;
-
-        anim.Rebind();
-        anim.enabled = false;
     }
     void Init()
     {
         isDone = false;
-        isFirst = true;
         curIdx = 0;
+        isFirst = true;
+        isScarecrow = false;
+        isJokbo = false;
+
         GameManager.instance.player.isAttackable = false;
     }
     void SetUPUI()
     {
-        npcName = UIManager.instance.SceneUI["Dialogue"].GetComponent<DialogueUIGroup>().childUI[1].GetComponent<TextMeshProUGUI>();
-        dialogueTxt = UIManager.instance.SceneUI["Dialogue"].GetComponent<DialogueUIGroup>()
-            .childUI[2].GetComponentInChildren<TextMeshProUGUI>(true);
+        dialogueTxt = childUI[0].GetComponentInChildren<TextMeshProUGUI>(true);
     }
 
     public void LoadTutorialEvent()
     {
         // 튜토리얼에 대한 대화는 여기서 따로 관리하는 식으로 진행 <-> dialogueInteraction
         dialogues = UIManager.instance.dialogueDB;
-        UIManager.instance.SceneUI["Dialogue"].SetActive(true);
+        Debug.Log("dd");
+        ToggleUI(childUI[0].gameObject);
+
         Init();
 
         for (int i = 0; i < dialogues.DialogueEntity.Count; i++)
@@ -83,92 +77,133 @@ public class TutorialUIGroup : UIGroup
             }
         }
 
-        if (isFirst)
-        {
+        if(isFirst)
             SetUPUI();
-        }
+
         StartCoroutine(ManageEvent());
     }
     IEnumerator ManageEvent()
     {
-        curIdx = SetNextDialog(curIdx);
-        SetActiveDialogUI(true);    // 첫 대화 세팅
-        yield return new WaitUntil(() => !isFirst);
+        yield return new WaitUntil(() => SetTutoTxt_1());
 
-        //if (!isWASD && UIManager.instance.fade.fadePanel.color.a < 0.1) anim.SetBool("isWASD", true);
+        yield return new WaitUntil(() => SetTutoTxt_3());
 
-        yield return new WaitUntil(() => UpdateDialogue());
-
-        UIManager.instance.SceneUI["Dialogue"].SetActive(false);
+        ToggleUI(childUI[0].gameObject);
     }
-    bool UpdateDialogue()
-    {   
-        if (Input.GetKeyDown(KeyCode.Escape)) isDone = true;     // 대화 도중 나갈 수 있습니다.
-        if (isDone) SetActiveDialogUI(false);
-        else
-        {
-            if (Input.GetKeyDown(KeyCode.F))
-            {   //  일반 대화 출력
-                if (dialogDatas.Count == curIdx) isDone = true;
-                else curIdx = SetNextDialog(curIdx);
-            }
-        }
 
-        return isDone;
-    }
-    void SetActiveDialogUI(bool visible)
-    {   // manage dialogue UI
-        dialogueTxt.gameObject.SetActive(visible);
-        if (isFirst) isFirst = false;
-    }
-    int SetNextDialog(int idx)
+   void SetNextDialog(int idx)
     {
         if (dialogDatas.Count > idx)
         {
-            npcName.text = dialogDatas[idx]._npcName;
-
-            if (isFirst)
-            {   // 첫 대화 출력
-                dialogueTxt.text = dialogDatas[idx]._dialogue;
-                if (!dialogDatas[idx]._isSelect) idx++;
-            }
-            
-            else
-            {   // 이외 일반 대화 출력 ---> 분기에 맞게 조건화 필요
-                dialogueTxt.text = dialogDatas[idx]._dialogue;
-                idx++;
-            }
-        }
-        return idx;
-    }
-    IEnumerator CheckStateCouroutine(string curType)
-    {   // UI 활성화 애니메이션이 끝나면 호출되는 코루틴, UI 비활성화 ON
-        yield return new WaitUntil(() => CheckState(curType));
-
-        if (curType == "Reload" && isReload)
-        {
-            anim.SetBool("isReload", false);
-        }
-        else if(curType == "Interaction" && isInteraction)
-        {
-            anim.SetBool("isInteraction", false);
-        }
-        else if (curType == "Skill" && isSkill)
-        {
-            anim.SetBool("isSkill", false);
-            anim.SetBool("isInteraction", true);
-            //anim.SetBool("isReload", true);
-        }
-        else if (curType == "Move" && isWASD)
-        {
-            GameManager.instance.player.isAttackable = true;
-            anim.SetBool("isWASD", false);
-            anim.SetBool("isSkill", true);
+            dialogueTxt.text = dialogDatas[idx]._dialogue;
         }
     }
 
-    public bool CheckState(string curType)
-    {   // 현재 진행 UI에 따라서 다음 애니메이션을 준비
+    float time = 0f;
+    bool SetTutoTxt_1()
+    {
+        SetNextDialog(curIdx);
+
+        switch (curIdx)
+        {
+            case 0: // welcome
+                time += Time.deltaTime;
+                if (time > waitTime)
+                {   // 3초의 대기시간 이후,
+                    Debug.Log("0-welcome");
+                    curIdx++;
+                    GameManager.instance.player.isAttackable = true;
+                }
+                break;
+            case 1: // move
+                if (CheckState("Move"))
+                {
+                    curIdx++;
+                    Debug.Log("1-move");
+                }
+                break;
+            case 2:
+                if (CheckState("AttacknDash"))
+                {
+                    curIdx++;
+                    Debug.Log("2-AttacknDash");
+                }
+                break;
+            case 3:
+                if (CheckState("Reload"))
+                {
+                    curIdx++;
+                    Debug.Log("3-reload");
+                }
+                break;
+            case 4:
+                if (CheckState("Interaction"))
+                {
+                    curIdx++;
+                    Debug.Log("4-interaction");
+                }
+                break;
+            default:    // 이외 일반 대화 출력
+                if (Input.GetButtonDown("Tutorial"))
+                {
+                    if (curIdx > 9)
+                    {
+                        return true;
+                    }
+                    curIdx++;
+                }
+                break;
+        }
+
+        return false;
+    }
+    bool SetTutoTxt_3()
+    {
+        SetNextDialog(curIdx);
+
+        switch (curIdx)
+        {
+            case 11: // GetJokbo - F
+                if (jokboInstantiate == null)
+                {
+                    jokboInstantiate = Instantiate(jokbo,
+                    (GameManager.instance.player.transform.position + Vector3.right),
+                    Quaternion.identity, scarecrowPos.parent);
+                }
+                else if (CheckState("GetJokbo"))
+                {
+                    curIdx++;
+                    Debug.Log("12 - isGetJokbo");
+                }
+                break;
+            case 12: // OpenJokbo
+                // 족보 설명 UI 열기
+                if (CheckState("OpenJokbo"))
+                {
+                    curIdx++;
+                    Debug.Log("13 - isOpenJokbo");
+                }
+                break;
+            case 16: // UseSkill - knockback
+                if (CheckState("Skill"))
+                {
+                    curIdx++;
+                    Debug.Log("17 - isSkill");
+                }
+                break;
+            default:    // 이외 일반 대화 출력
+                if (Input.GetKeyDown(KeyCode.F))
+                {
+                    curIdx++;
+                    if (dialogDatas.Count == curIdx) isDone = true;
+                }
+                break;
+        }
+        return isDone;   // true : 대화가 종료되어 해당 함수의 호출을 멈춤. false : 대화가 아직 종료되지 않아 해당 함수의 호출을 계속함.
+    }
+
+    bool CheckState(string curType)
+    {  
         if (!isWASD && curType == "Move" && (Input.GetKeyDown(KeyCode.W)
             || Input.GetKeyDown(KeyCode.A)
             || Input.GetKeyDown(KeyCode.S)
@@ -180,6 +215,7 @@ public class TutorialUIGroup : UIGroup
 
         else if (isWASD && !isAttack && Input.GetKeyDown(KeyCode.Mouse0))
         {
+            // todo ------- 허수아비를 맞췄다는 상태 확인 후 다음으로 넘어갈 수 있게 함
             isAttack = true;
             Debug.Log("isAttack");
         }
@@ -190,11 +226,10 @@ public class TutorialUIGroup : UIGroup
             Debug.Log("isDash");
         }
 
-        else if (isAttack && isDash && curType == "Skill")
+        else if (isAttack && isDash && curType == "AttacknDash")
         {
-            isSkill = true;
-            Debug.Log("isSkill");
-            return isSkill;
+            Debug.Log("isAttacknDash");
+            return isAttack | isDash;
         }
 
         else if (!isReload && curType == "Reload" && Input.GetKeyDown(KeyCode.R))
@@ -204,13 +239,59 @@ public class TutorialUIGroup : UIGroup
             return isReload;
         }
 
-        else if(!isInteraction && curType == "Interaction" && Input.GetKeyDown(KeyCode.F))
+        else if(!isInteraction && isScarecrow && curType == "Interaction" && Input.GetKeyDown(KeyCode.F))
         {
+            // 허수아비에게 가서 F키를 눌렀는가 체크
             isInteraction = true;
             Debug.Log("isInteraction");
             return isInteraction;
         }
 
+        // ---------- tuto3 : 섯다 족보 소개
+        else if(!isGetJokbo && isJokbo && curType == "GetJokbo")
+        {
+            Debug.Log("isGetJokbo");
+            return isGetJokbo = true;
+        }
+
+        else if(!isOpenJokbo && curType=="OpenJokbo" && Input.GetKeyDown(KeyCode.K))
+        {   // todo ------- UI가 닫혔는지 확인 후 다음으로 넘어갈 수 있게 함
+            Debug.Log("isOpenJokbo");
+            return isOpenJokbo = true;
+        }
+
+        else if (!isSkill && curType == "Skill" && Input.GetKeyDown(KeyCode.Q))
+        {   // todo ------- 넉백 스킬 사용을 햇다는 상태확인 후 다음으로 넘어갈 수 있게 함
+            Debug.Log("isSkill");
+            return isSkill = true;
+        }
+
         return false;
-    } 
+    }
+
+    //IEnumerator CheckStateCouroutine(string curType)
+    //{   // UI 활성화 애니메이션이 끝나면 호출되는 코루틴, UI 비활성화 ON
+    //    yield return new WaitUntil(() => CheckState(curType));
+
+    //    if (curType == "Reload" && isReload)
+    //    {
+    //        anim.SetBool("isReload", false);
+    //    }
+    //    else if(curType == "Interaction" && isInteraction)
+    //    {
+    //        anim.SetBool("isInteraction", false);
+    //    }
+    //    else if (curType == "Skill" && isSkill)
+    //    {
+    //        anim.SetBool("isSkill", false);
+    //        anim.SetBool("isInteraction", true);
+    //        //anim.SetBool("isReload", true);
+    //    }
+    //    else if (curType == "Move" && isWASD)
+    //    {
+    //        GameManager.instance.player.isAttackable = true;
+    //        anim.SetBool("isWASD", false);
+    //        anim.SetBool("isSkill", true);
+    //    }
+    //}
 }
