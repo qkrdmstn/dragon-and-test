4c04edcdb8e41c9e6a11a5e2ce1851484b9a205b
Sieun Choi <tldmschl81@soongsil.ac.kr>
2024-06-16 20:39:07 +0900

    [] soundManager prefab -> battle scenes 적용 [] 미니맵 UI in battleA

---
+++ Assets/Scripts/System/UI/MapIndicator.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum MapType
+{
+    A, B, C
+}
+
+public class MapIndicator : MonoBehaviour
+{
+    [SerializeField] MapType myType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitializeMiniMap();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void InitializeMiniMap()
+    {
+        
+    }
+}
