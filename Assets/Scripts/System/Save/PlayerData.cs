486cc16b134dd7399803ecc43abfb315e7cd985a
Sieun Choi <tldmschl81@soongsil.ac.kr>
2024-07-18 02:32:53 +0900

    새로운 에셋 추가 - 구르기 사운드, 글씨체, 마을 배경, 저장 관련 스크립트

---
+++ Assets/Scripts/System/Save/PlayerData.cs
@@ -0,0 +1,44 @@
+using System;
+
+[Serializable]
+public class PlayerData
+{
+    public int totalPlayTime;   // mins->parse필요
+    public Date date;
+    public string chapterName;
+
+    int playerHP;
+    int playerMP;
+    int[] skills;
+    int[] hwatus;
+    int money;
+
+    int curGun;
+    int curBullets;
+    int curShields;
+
+    public PlayerData()
+    {
+        skills = new int[2];
+        hwatus = new int[10];
+    }
+}
+
+public struct Date
+{
+    int year;
+    int month;
+    int day;
+    int hour;
+    int min;
+
+    public Date(int _year, int _month, int _day, int _hour, int _min)
+    {
+        year = _year;
+        month = _month;
+        day = _day;
+        hour = _hour;
+        min = _min;
+    }
+}
+
