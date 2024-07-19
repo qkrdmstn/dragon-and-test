3cfd29ae70376d019b4a56e5e808fb12898f51b2
Park Eunsu <qkrdmstn1258@naver.com>
2024-07-20 04:57:58 +0900

    모포 drag 문제 해결

--- Assets/Scripts/Skill/SkillUI/MaterialHwatuSlotUI.cs
+++ Assets/Scripts/Skill/SkillUI/MaterialHwatuSlotUI.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using DG.Tweening;
+using Unity.VisualScripting;
 
 public class MaterialHwatuSlotUI : MonoBehaviour
     , IPointerEnterHandler
@@ -23,6 +24,7 @@ public class MaterialHwatuSlotUI : MonoBehaviour
     public Vector3 dragOffset;
     public bool isSelected = false;
     public bool isTweening = false;
+    public bool isDrag = false;
 
     private Image image;
 
@@ -34,22 +36,22 @@ public class MaterialHwatuSlotUI : MonoBehaviour
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        if(!isTweening && !isSelected)
-        {
-            originSiblingIndex = transform.GetSiblingIndex(); // 가장 위에 렌더링되는 카드가 자연스럽도록 Sibling의 순서를 바꿈
-            transform.SetAsLastSibling();
+        if (isSelected || isTweening || isDrag)
+            return;
 
-            MoveTransform(originPos + new Vector3(0, 100, 0), originRot, originScale, 0.25f, false);
-        }
+        originSiblingIndex = transform.GetSiblingIndex(); // 가장 위에 렌더링되는 카드가 자연스럽도록 Sibling의 순서를 바꿈
+        transform.SetAsLastSibling();
+
+        MoveTransform(originPos + new Vector3(0, 100, 0), originRot, originScale, 0.25f);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        if (!isTweening && !isSelected)
-        {
-            transform.SetSiblingIndex(originSiblingIndex);
-            MoveTransform(0.25f, false);
-        }
+        if (isSelected || isTweening || isDrag)
+            return;
+
+        transform.SetSiblingIndex(originSiblingIndex);
+        MoveTransform(0.25f);
     }
 
     public void OnPointerClick(PointerEventData eventData)
@@ -84,17 +86,19 @@ public class MaterialHwatuSlotUI : MonoBehaviour
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        if (isSelected)
+        if (isSelected || isTweening)
             return;
 
+        isDrag = true;
         dragOffset = transform.position - Input.mousePosition;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        if (isSelected)
+        if (isSelected || isTweening)
             return;
 
+        isDrag = false;
         RectTransform rectTransform = this.GetComponent<RectTransform>();
         BlanketUI blanketUI = transform.GetComponentInParent<BlanketUI>();
         bool isInBlanket = blanketUI.IsInBlanket(rectTransform);
@@ -105,7 +109,7 @@ public class MaterialHwatuSlotUI : MonoBehaviour
             BlanketInteraction blanketInteraction = FindObjectOfType<BlanketInteraction>();
             bool isCombinationPossible = blanketInteraction.AddSelectedHwatu(this); //조합 체크
 
-            if(isCombinationPossible) //가능한 조합
+            if (isCombinationPossible) //가능한 조합
             {
                 Vector3 pos = Input.mousePosition + dragOffset;
                 Quaternion rot = Quaternion.identity;
@@ -120,7 +124,7 @@ public class MaterialHwatuSlotUI : MonoBehaviour
                 transform.SetSiblingIndex(originSiblingIndex);
             }
         }
-        else if(isInTrashCan) //쓰레기통에 겹치면
+        else if (isInTrashCan) //쓰레기통에 겹치면
         {
             BlanketInteraction blanketInteraction = FindObjectOfType<BlanketInteraction>();
             blanketInteraction.DeleteHwatu(this);
@@ -134,16 +138,16 @@ public class MaterialHwatuSlotUI : MonoBehaviour
 
     public void OnDrag(PointerEventData eventData)
     {
-        if (isTweening)
+        if (isSelected || isTweening)
             return;
 
-        if (!isSelected)
-        {
-            Vector3 pos = Input.mousePosition + dragOffset;
-            Quaternion rot = Quaternion.identity;
-            Vector3 scale = new Vector3(2, 2, 1);
-            MoveTransform(pos, rot, scale);
-        }
+        if (!isDrag)
+            isDrag = true;
+
+        Vector3 pos = Input.mousePosition + dragOffset;
+        Quaternion rot = Quaternion.identity;
+        Vector3 scale = new Vector3(2, 2, 1);
+        MoveTransform(pos, rot, scale);
     }
 
     public void MoveTransform(Vector3 pos, Quaternion rot, Vector3 scale, float dotweenTime = 0, bool tweenSetting = true)
