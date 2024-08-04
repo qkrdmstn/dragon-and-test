2cb797a5325f11ec7fe7542b67275ce8e6a5c78b
Sieun Choi <tldmschl81@soongsil.ac.kr>
2024-08-05 02:29:47 +0900

    마우스 방향에 따른 이무기 얼굴 스킨 갱신

--- Assets/Scripts/Player/AnimController.cs
+++ Assets/Scripts/Player/AnimController.cs
@@ -1,8 +1,7 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Spine.Unity;
 using Spine;
+using Spine.Unity;
 using Spine.Unity.AttachmentTools;
 
 public enum AnimState
@@ -12,14 +11,12 @@ public enum AnimState
 
 public enum Direction
 {
-	Back, Back_L, Back_R,
-	Front, Front_L, Front_R
+	BACK, BACK_L, BACK_R,
+	FRONT, FRONT_L, FRONT_R
 }
 
 public class AnimController : MonoBehaviour
 {
-	SkeletonAnimation skeletonAnimation;
-
 	// character skins
 	[SpineSkin] public string baseSkinName;
 	[SpineSkin] public string[] idleSkin;
@@ -32,8 +29,7 @@ public class AnimController : MonoBehaviour
 	string curAnim;
 
 	public bool isBreath = false;
-	int curDirectionIdx = -1;
-	int curBaseSkinIdx = -1;
+	Direction curDirectionIdx, curBaseSkinIdx;
 
 	[System.Serializable]
 	public class SlotRegionPair
@@ -42,12 +38,14 @@ public class AnimController : MonoBehaviour
 		[SpineAtlasRegion] public string region;
 	}
 
-	[SerializeField] protected SpineAtlasAsset atlasAsset;
-	[SerializeField] protected bool inheritProperties = true;
-	[SerializeField] protected List<SlotRegionPair> attachments = new List<SlotRegionPair>();
+	[SerializeField] SpineAtlasAsset atlasAsset;
+	[SerializeField] bool inheritProperties = true;
+	[SerializeField] List<SlotRegionPair> attachments = new List<SlotRegionPair>();
 
 	Atlas atlas;
+	SkeletonAnimation skeletonAnimation;
 	SkeletonRenderer skeletonRenderer;	// 피격 시 깜빡임
+
 	float mouseDirMinX = -0.2f;
 	float mouseDirMaxX = 0.2f;
 	float mouseDirMinY = -0.2f;
@@ -57,143 +55,133 @@ public class AnimController : MonoBehaviour
 	{
 		skeletonRenderer = GetComponent<SkeletonRenderer>();
 		skeletonAnimation = GetComponent<SkeletonAnimation>();
-
-		skeletonRenderer.OnRebuild += Apply;
-	}
-
-	private void Update()
-	{
-		if (skeletonRenderer.valid && isBreath) Apply(skeletonRenderer);
 	}
 
 	public void SetAnim(AnimState animState, float x = 0f, float y = 0f)
 	{
 		if (animState == AnimState.Idle)
         {
-			if (x >= mouseDirMaxX && y >= mouseDirMaxY) curBaseSkinIdx = (int)Direction.Back_R;
-			else if (x >= mouseDirMinX && x < mouseDirMaxX && y >= mouseDirMaxY) curBaseSkinIdx = (int)Direction.Back;
-			else if (x >= mouseDirMaxX && y < mouseDirMaxY && y >= mouseDirMinY) curBaseSkinIdx = (int)Direction.Front_R;
-			else if (x >= mouseDirMaxX && y < mouseDirMinY) curBaseSkinIdx = (int)Direction.Front_R;
-			else if (x >= mouseDirMinX && x < mouseDirMaxX && y < mouseDirMaxY) curBaseSkinIdx = (int)Direction.Front;
-			else if (x < mouseDirMinX && y < mouseDirMaxY && y >= mouseDirMinY) curBaseSkinIdx = (int)Direction.Front_L;
-			else if (x < mouseDirMinX && y < mouseDirMinY) curBaseSkinIdx = (int)Direction.Front_L;
-			else if (x < mouseDirMinX && y >= mouseDirMaxY) curBaseSkinIdx = (int)Direction.Back_L;
+			if (x >= mouseDirMaxX && y >= mouseDirMaxY) curBaseSkinIdx = Direction.BACK_R;
+			else if (x >= mouseDirMinX && x < mouseDirMaxX && y >= mouseDirMaxY) curBaseSkinIdx = Direction.BACK;
+			else if (x >= mouseDirMaxX && y < mouseDirMaxY && y >= mouseDirMinY) curBaseSkinIdx = Direction.FRONT_R;
+			else if (x >= mouseDirMaxX && y < mouseDirMinY) curBaseSkinIdx = Direction.FRONT_R;
+			else if (x >= mouseDirMinX && x < mouseDirMaxX && y < mouseDirMaxY) curBaseSkinIdx = Direction.FRONT;
+			else if (x < mouseDirMinX && y < mouseDirMaxY && y >= mouseDirMinY) curBaseSkinIdx = Direction.FRONT_L;
+			else if (x < mouseDirMinX && y < mouseDirMinY) curBaseSkinIdx = Direction.FRONT_L;
+			else if (x < mouseDirMinX && y >= mouseDirMaxY) curBaseSkinIdx = Direction.BACK_L;
 		}
 		else if (animState == AnimState.Run || animState == AnimState.Wave)
         {
-			if (x == 1 && y == 1) curBaseSkinIdx = (int)Direction.Back_R;
-			else if (x == 0 && y == 1) curBaseSkinIdx = (int)Direction.Back;
-			else if (x == 1 && y == 0) curBaseSkinIdx = (int)Direction.Front_R;
-			else if (x == 1 && y == -1) curBaseSkinIdx = (int)Direction.Front_R;
-			else if (x == 0 && y == 0) curBaseSkinIdx = (int)Direction.Front;
-			else if (x == 0 && y == -1) curBaseSkinIdx = (int)Direction.Front;
-			else if (x == -1 && y == -1) curBaseSkinIdx = (int)Direction.Front_L;
-			else if (x == -1 && y == 0) curBaseSkinIdx = (int)Direction.Front_L;
-			else if (x == -1 && y == 1) curBaseSkinIdx = (int)Direction.Back_L;
+			if (x == 1 && y == 1) curBaseSkinIdx = Direction.BACK_R;
+			else if (x == 0 && y == 1) curBaseSkinIdx = Direction.BACK;
+			else if (x == 1 && y == 0) curBaseSkinIdx = Direction.FRONT_R;
+			else if (x == 1 && y == -1) curBaseSkinIdx = Direction.FRONT_R;
+			else if (x == 0 && y == 0) curBaseSkinIdx = Direction.FRONT;
+			else if (x == 0 && y == -1) curBaseSkinIdx = Direction.FRONT;
+			else if (x == -1 && y == -1) curBaseSkinIdx = Direction.FRONT_L;
+			else if (x == -1 && y == 0) curBaseSkinIdx = Direction.FRONT_L;
+			else if (x == -1 && y == 1) curBaseSkinIdx = Direction.BACK_L;
 		}
 
 		switch (animState)
 		{
 			case AnimState.Idle:
-				baseSkinName = idleSkin[curBaseSkinIdx];
+				baseSkinName = idleSkin[(int)curBaseSkinIdx];
 				curAnim = idleAnim;
 				break;
 
 			case AnimState.Run:
-				baseSkinName = runSkin[curBaseSkinIdx];
+				baseSkinName = runSkin[(int)curBaseSkinIdx];
 				curAnim = runAnim;
 				break;
 
 			case AnimState.Wave:
-				baseSkinName = waveSkin[curBaseSkinIdx];
+				baseSkinName = waveSkin[(int)curBaseSkinIdx];
 				curAnim = waveAnim;
 				break;
 		}
 
-		if (isBreath)
-		{
-			skeletonAnimation.AnimationName = curAnim;
-			skeletonAnimation.ApplyAnimation();
-			return;
-		}
-
-		skeletonAnimation.initialSkinName = baseSkinName;
-		skeletonAnimation.Skeleton.SetSkin(baseSkinName);
+		skeletonAnimation.skeleton.SetSkin(baseSkinName);
 		skeletonAnimation.Skeleton.SetSlotsToSetupPose();
+
+		if (isBreath || animState == AnimState.Run)
+        {	// 총을 쏘거나 마우스 이동의 얼굴 방향 및 표정 스킨 갱신
+			ChangeSkinSlot(SetMouseDirection(Player.instance.stateMachine.currentState.mouseDir.x, Player.instance.stateMachine.currentState.mouseDir.y));
+		}
 
 		skeletonAnimation.AnimationName = curAnim;
 		skeletonAnimation.ApplyAnimation();
 	}
 
-	[SpineAtlasRegion] public string[] idleFace;
-	[SpineAtlasRegion] public string[] runFace;
+	void ChangeSkinSlot(Direction _region)
+    {
+		atlas = atlasAsset.GetAtlas();
+		float scale = skeletonRenderer.skeletonDataAsset.scale;
 
-	public void SetBreath(float x, float y)
+		Slot slot;
+		AtlasRegion region;
+
+		if (isBreath)
+		{
+			if (Player.instance.stateMachine.currentState == Player.instance.idleState)
+			{
+				attachments[0].slot = "IDLE_basicface";
+			}
+			else if (Player.instance.stateMachine.currentState == Player.instance.moveState)
+			{
+				attachments[0].slot = "RUN_basicface";
+			}
+
+			slot = skeletonRenderer.skeleton.FindSlot(attachments[0].slot);
+			region = atlas.FindRegion(_region.ToString() + "/Face_Bress");
+		}
+		else
+		{
+			slot = skeletonRenderer.skeleton.FindSlot(attachments[1].slot);
+
+			switch (_region)
+			{
+				case Direction.FRONT_R:
+					region = atlas.FindRegion(_region.ToString() + "/FACE");
+					break;
+				default:
+					region = atlas.FindRegion(_region.ToString() + "/Face");
+					break;
+			}
+		}
+
+		Attachment originAttachment = slot.Attachment;
+		slot.Attachment = originAttachment.GetRemappedClone(region, true, true, scale);
+	}
+
+	Direction SetMouseDirection(float x, float y)
     {
 		if (x >= mouseDirMaxX && y >= mouseDirMaxY)
-			curDirectionIdx = (int)Direction.Back_R;
-		else if (x >= mouseDirMinX && x < mouseDirMaxX && y >= mouseDirMaxY)
-			curDirectionIdx = (int)Direction.Back;
-		else if (x >= mouseDirMaxX && y < mouseDirMaxY && y >= mouseDirMinY)
-			curDirectionIdx = (int)Direction.Front_R;
-		else if (x >= mouseDirMaxX && y < mouseDirMinY)
-			curDirectionIdx = (int)Direction.Front_R;
-		else if (x >= mouseDirMinX && x < 60 && y < mouseDirMaxY)
-			curDirectionIdx = (int)Direction.Front;
-		else if (x < mouseDirMinX && y < mouseDirMaxY && y >= mouseDirMinY)
-			curDirectionIdx = (int)Direction.Front_L;
-		else if (x < mouseDirMinX && y < mouseDirMinY)
-			curDirectionIdx = (int)Direction.Front_L;
-		else if (x < mouseDirMinX && y >= mouseDirMaxY)
-			curDirectionIdx = (int)Direction.Back_L;
+			curDirectionIdx = Direction.BACK_R;
 
-		if (Player.instance.stateMachine.currentState == Player.instance.idleState)
-        {
-			isBreath = true;
-			attachments[0].slot = "IDLE_basicface";
-			attachments[0].region = idleFace[curDirectionIdx];
-		}
-		else if(Player.instance.stateMachine.currentState == Player.instance.moveState)
-        {
-			isBreath = true;
-			attachments[0].slot = "RUN_basicface";
-			attachments[0].region = runFace[curDirectionIdx];
-		}
-    }
+		else if (x >= mouseDirMinX && x < mouseDirMaxX && y >= mouseDirMaxY)
+			curDirectionIdx = Direction.BACK;
+
+		else if (x >= mouseDirMaxX && y < mouseDirMaxY)
+			curDirectionIdx = Direction.FRONT_R;
+
+		else if (x >= mouseDirMinX && x < mouseDirMaxX && y < mouseDirMaxY)
+			curDirectionIdx = Direction.FRONT;
+
+		else if (x < mouseDirMinX && y < mouseDirMaxY && y >= mouseDirMinY)
+			curDirectionIdx = Direction.FRONT_L;
+		else if (x < mouseDirMinX && y < mouseDirMinY)
+			curDirectionIdx = Direction.FRONT_L;
+
+		else if (x < mouseDirMinX && y >= mouseDirMaxY)
+			curDirectionIdx = Direction.BACK_L;
+
+		return curDirectionIdx;
+	}
 
 	public void SetMaterialColor(Color color)
     {
 		skeletonAnimation.skeleton.SetColor(color);
-	}
-
-	void Apply(SkeletonRenderer skeletonRenderer)
-	{
-		if (!this.enabled) return;
-
-		atlas = atlasAsset.GetAtlas();
-		if (atlas == null) return;
-		float scale = skeletonRenderer.skeletonDataAsset.scale;
-
-		foreach (SlotRegionPair entry in attachments)
-		{
-			Slot slot = skeletonRenderer.Skeleton.FindSlot(entry.slot);
-			Attachment originalAttachment = slot.Attachment;
-			AtlasRegion region = atlas.FindRegion(entry.region);
-
-			if (region == null)
-			{
-				slot.Attachment = null;
-			}
-			else if (inheritProperties && originalAttachment != null)
-			{
-				slot.Attachment = originalAttachment.GetRemappedClone(region, true, true, scale);
-			}
-			else
-			{
-				RegionAttachment newRegionAttachment = region.ToRegionAttachment(region.name, scale);
-				slot.Attachment = newRegionAttachment;
-			}
-		}
 	}
 }
 
