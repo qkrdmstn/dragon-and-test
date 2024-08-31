35de4c3aa7d9f9ca206b1bd155e0047c9de1b825
Sieun Choi <tldmschl81@soongsil.ac.kr>
2024-08-31 20:28:41 +0900

    미니맵 제작중

--- Assets/Scripts/System/Map/MapIndicator.cs
+++ Assets/Scripts/System/Map/MapIndicator.cs
@@ -1,64 +1,82 @@
+using UnityEngine;
+using UnityEngine.UI;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using UnityEngine;
-
 public class MapIndicator : MonoBehaviour
 {
-    [SerializeField] GameObject playerUI;
-    BlockUIInfo[] blockUIInfos;
+    public GameObject playerUI;
+    public GameObject mapUI;
+    public GameObject shopUI;
+    public GameObject blanketUI;
+    RectTransform playerRect, mapRect, shopRect, blanketRect;
+    List<RectTransform> mapRects;
+    BlockInfo[] blocks;
     bool[] isVisited;
     public int firstBlock;
+    [Range(0, 5f)]public float size;
 
-    void Awake()
+    private void Awake()
     {
-        blockUIInfos = GetComponentsInChildren<BlockUIInfo>();
-        Array.Sort(blockUIInfos);   // ordered by pos
+    }
+    private void OnValidate()
+    {
+        mapRect = mapUI.GetComponent<RectTransform>();
+        blocks = FindObjectsOfType<BlockInfo>();
+        isVisited = new bool[blocks.Length];
 
-        isVisited = new bool[blockUIInfos.Length];
-     }
+        for (int i = 0; i < blocks.Length; i++)
+        {
+            PolygonCollider2D tmpColl = blocks[i].GetComponentInChildren<PolygonCollider2D>();
+            Vector3 rectViewPos = blocks[i].transform.position * size;
+
+            RectTransform tmp = Instantiate(mapRect, rectViewPos + transform.position, Quaternion.identity, this.transform);
+            tmp.name = "blockUI - " + i;
+            tmp.sizeDelta = tmpColl.bounds.size * size;
+        }
+    }
+
     void Start()
     {
-        MoveBlockPlayer(firstBlock);  // First Setting
+        //MoveBlockPlayer(firstBlock);  // First Setting
+        
     }
+    //public void MoveBlockPlayer(int goToBlockNum)   // goToBlockNum is a Hierarchy Order
+    //{   
+    //    // Set a Player UI
+    //    playerUI.transform.SetParent(blockUIInfos[goToBlockNum].transform);
+    //    playerUI.transform.localPosition = Vector3.one;
 
-    public void MoveBlockPlayer(int goToBlockNum)   // goToBlockNum is a Hierarchy Order
-    {   
-        // Set a Player UI
-        playerUI.transform.SetParent(blockUIInfos[goToBlockNum].transform);
-        playerUI.transform.localPosition = Vector3.one;
+    //    if (isVisited[goToBlockNum]) return;
+    //    isVisited[goToBlockNum] = true;
 
-        if (isVisited[goToBlockNum]) return;
-        isVisited[goToBlockNum] = true;
+    //    // Inactive a Question mark
+    //    blockUIInfos[goToBlockNum].SwitchChild(BlockChild.Question, false);
 
-        // Inactive a Question mark
-        blockUIInfos[goToBlockNum].SwitchChild(BlockChild.Question, false);
+    //    ShowNextBlock(goToBlockNum);
+    //}
 
-        ShowNextBlock(goToBlockNum);
-    }
+    //void ShowNextBlock(int _blockOrder)
+    //{   // Active Next Blocks & bridges
+    //    if (blockUIInfos[_blockOrder].nextBlock.Length == 0) return;
 
-    void ShowNextBlock(int _blockOrder)
-    {   // Active Next Blocks & bridges
-        if (blockUIInfos[_blockOrder].nextBlock.Length == 0) return;
+    //    if (blockUIInfos[_blockOrder].isWay) blockUIInfos[_blockOrder].SwitchChild(BlockChild.Way, true);
+    //    foreach (BlockUIInfo nextBlockUI in blockUIInfos[_blockOrder].nextBlock)
+    //    {
+    //        nextBlockUI.SwitchChild(BlockChild.BG, true);
+    //        nextBlockUI.SwitchChild(BlockChild.Question, true);
+    //    }
+    //}
+    //public void BlockClear(int curBlock)
+    //{   // Active a Next Interaction Object - shop, boss, blanket
+    //    if (blockUIInfos[curBlock].nextBlock.Length == 0) return;
 
-        if (blockUIInfos[_blockOrder].isWay) blockUIInfos[_blockOrder].SwitchChild(BlockChild.Way, true);
-        foreach (BlockUIInfo nextBlockUI in blockUIInfos[_blockOrder].nextBlock)
-        {
-            nextBlockUI.SwitchChild(BlockChild.BG, true);
-            nextBlockUI.SwitchChild(BlockChild.Question, true);
-        }
-    }
-    public void BlockClear(int curBlock)
-    {   // Active a Next Interaction Object - shop, boss, blanket
-        if (blockUIInfos[curBlock].nextBlock.Length == 0) return;
+    //    foreach (BlockUIInfo nextBlockUI in blockUIInfos[curBlock].nextBlock)
+    //    {
+    //        if(!nextBlockUI.isInteraction) continue;
 
-        foreach (BlockUIInfo nextBlockUI in blockUIInfos[curBlock].nextBlock)
-        {
-            if(!nextBlockUI.isInteraction) continue;
-
-            nextBlockUI.SwitchChild(BlockChild.Question, false);
-            nextBlockUI.SwitchChild(BlockChild.Interaction, true);
-        }
-    }
+    //        nextBlockUI.SwitchChild(BlockChild.Question, false);
+    //        nextBlockUI.SwitchChild(BlockChild.Interaction, true);
+    //    }
+    //}
 }
